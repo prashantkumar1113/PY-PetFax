a11c01a3acd2d19eef8a63739e1164067397bf9f
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-06-27 18:54:38 -0500

    Created some test routes with flask

---
+++ app.py
@@ -0,0 +1,15 @@
+# config
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, this is PetFax!"
+
+@app.route('/pets')
+def pets():
+    return "These are our pets available for adoption"
+
+@app.route('/test')
+def test():
+    return {"fname": "Prashant", "lname": "Kumar"}
