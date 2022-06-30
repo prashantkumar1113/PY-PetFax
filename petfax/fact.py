2ec01c792ef60f9342caa689bd8e8dac578d539c
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-06-29 20:53:05 -0500

    finished interacting with the db and displaying data

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,15 +1,27 @@
-from crypt import methods
-import imp
+from operator import mod
 from flask import (Blueprint, render_template, request, redirect)
+from . import models
+
 bp = Blueprint('fact', __name__, url_prefix='/facts')
 
 @bp.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         print(request.form)
+        submitter = request.form["submitter"]
+        fact = request.form["fact"]
+
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
         redirect('/facts')
+
     # return "This is the facts index"
-    return render_template("facts/index.html")
+    results = models.Fact.query.all()
+    # for fact in results:
+    #     print(f"submitter: {fact.submitter}, fact:{fact.fact}")
+    return render_template("facts/index.html", facts=results)
 
 
 @bp.route("/new")
